5f6e2198d9aa84550a82ebdcc618f010cd309214
Rick Levy <ricklevy21@gmail.com>
2020-06-28 14:12:28 -0600

    jawsdb heroku deployment set up

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,30 @@
 //Set up MySQL connection
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "hunter21",
-    database: "restaurant_db"
-});
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  var connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "hunter21",
+      database: "restaurant_db"
+  });
+}
+
 
 //make connection
-connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
+connection.connect();
+
+
+// connection.connect(function(err) {
+//     if (err) {
+//       console.error("error connecting: " + err.stack);
+//       return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+//   });
 
 // Export connection for our ORM to use.
 module.exports = connection;
