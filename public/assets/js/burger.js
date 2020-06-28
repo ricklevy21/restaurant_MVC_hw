16091cc5008563ae463cfb2ea9ff2f26bccc4c92
Rick Levy <ricklevy21@gmail.com>
2020-06-28 12:44:02 -0600

    created burger.js in public folder. adds ajax functionality to front end

---
+++ public/assets/js/burger.js
@@ -0,0 +1,52 @@
+$(function(){
+    $(".create-form").on("submit", function(event){
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#newburger").val().trim(),
+            devoured: 0
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(function(){
+            console.log("new hamburger added");
+            //reload the page so the new burger is diplayed on the page
+            location.reload();
+        });
+    });
+
+    $(".eatburger").on("click", function(event){
+        event.preventDefault();
+
+        var id = $(this).data("id");
+        var devouredState = {
+            devoured: 1
+        };
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: devouredState
+        }).then(function(){
+            console.log("You ate this hamburger");
+            //reload the page so the new burger is diplayed on the page
+            location.reload();
+        });
+    });
+
+    $(".trashburger").on("click", function(event){
+        event.preventDefault();
+
+        var id = $(this).data("id");
+
+        //delete the burger
+        $.ajax({
+            type: "DELETE",
+            url: "/api/burgers/" + id
+        }).then(function(){
+            console.log("you threw away the hamburger")
+            location.reload();
+        });
+    });
+
+});
